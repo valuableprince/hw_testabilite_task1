7aeda399cf68d5fb1fe184f29dd8bb98b08e1263
Dmitry Ivanov <dmitriy_ivanov98@mail.ru>
2026-01-30 19:58:31 +0300

    Update code

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class BonusMilesService {
+public class BonusMilesService {
+    public int calculate(int price) {
+        int costBonus = 20;
+        int bonusMiles = price / costBonus;
+        return bonusMiles;
+    }
 }
